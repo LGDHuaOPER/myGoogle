0c120eb996a71ffe91f6502e5d2f3a5639ada1da
shiconghua <shiconghua@cmss.chinamobile.com>
2020-02-27 16:49:39 +0800

    “update-at-202002271649"

--- down&uploadGit.sh
+++ down&uploadGit.sh
@@ -3,17 +3,23 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# step1-添加
+# step1-拉取代码
+echo "- git pull"
+git pull origin master
+# -e 开启转义
+echo -e "  - git pull success! \n"
+
+# step2-添加
 echo "- git add"
 git add -A
 echo -e "  - git add success! \n"
 
-# step2-提交到历史区，$1 为运行 sh 时的第一个参数
+# step3-提交到历史区，$1 为运行 sh 时的第一个参数
 echo "- git commit"
 git commit -m $1
 echo -e "  - git commit success! \n"
 
-# step3-提交到 master 分支
+# step4-提交到 master 分支
 echo "- git push"
 git push origin master
 echo -e "  - git push success! \n"
